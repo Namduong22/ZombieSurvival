754e432cf39e4653a77b913abe77b16580530613
NamDuong <namduong220203@gmail.com>
2024-11-08 17:17:48 +0700

    Add BlasterGun, remove weapons and items
    Add BlasterGun, vật phẩm biến mất sau một khoảng thời gian tồn tại, nhân vật mất súng đã nhặt nếu bị zombie tấn công

--- Assets/Scripts/Game/Collectables/Collectable.cs
+++ Assets/Scripts/Game/Collectables/Collectable.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    private ICollectableBehaviour _collectableBehaviour;
+	[SerializeField]
+	private float _expiryTime;
+
+	private SpriteFlash _spriteFlash;
+	private ICollectableBehaviour _collectableBehaviour;
 
     private void Awake()
     {
-        _collectableBehaviour = GetComponent<ICollectableBehaviour>();
+		_spriteFlash = GetComponent<SpriteFlash>();
+		_collectableBehaviour = GetComponent<ICollectableBehaviour>();
     }
-   private void OnTriggerEnter2D(Collider2D collision)
+
+	private void OnEnable()
+	{
+		Invoke(nameof(StartExpiry), _expiryTime);
+	}
+
+	private void StartExpiry()
+	{
+		if (isActiveAndEnabled)
+		{
+			StartCoroutine(ExpiryCoroutine());
+		}
+	}
+
+	private IEnumerator ExpiryCoroutine()
+	{
+		yield return _spriteFlash.FlashCoroutine(3, new Color(1, 1, 1, 0.5f), 7);
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<PlayerMovement>();
 
--- Assets/Scripts/Game/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Game/Enemy/EnemyAttack.cs
@@ -7,13 +7,35 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField]
     private float _damageAmount;
 
+	[SerializeField]
+	private GameObject _bulletPrefab;
+
+	[SerializeField]
+	private float _bulletSpeed;
+
+	[SerializeField]
+	private float _timeBetweenShots;
+
 	private void OnCollisionStay2D(Collision2D collision)
 	{
 		if (collision.gameObject.GetComponent<PlayerMovement>())
 		{
 			var healthController = collision.gameObject.GetComponent<HealthController>();
 
+			float currentHealth = healthController._currentHealth;
+
 			healthController.TakeDamage(_damageAmount);
+
+			if (healthController._currentHealth < currentHealth)
+			{
+				var playerShoot = collision.gameObject.GetComponent<PlayerShoot>();
+				if (playerShoot != null)
+				{
+					playerShoot.SetBulletPrefab(_bulletPrefab);
+					playerShoot.SetBulletSpeed(_bulletSpeed);
+					playerShoot.SetTimeBetweenShots(_timeBetweenShots);
+				}
+			}
 		}
 	}
 }
